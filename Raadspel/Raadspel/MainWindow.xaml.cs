55f454f64308e981e93d382d5da9b0c5e76f8a37
MathiasRobbensPXL <Mathias.Robbens@student.pxl.be>
2024-10-25 09:18:57 +0200

    Added Number Generator and the Checks with Corresponding messages.

--- Raadspel/Raadspel/MainWindow.xaml.cs
+++ Raadspel/Raadspel/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,53 @@ namespace Raadspel
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void evaluateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(inputTextBox.Text, out int input1);
+
+            if (!numberGenerated)
+            {
+
+
+                Random rnd = new Random();
+                rnd.Next(1, 100);
+                numberGenerated = true;
+            }
+            else
+            {
+
+
+                if (input1 == rnd.Next(1, 100))
+                {
+                    output1TextBox.Text = "Congrats u guessed the number";
+                }
+                else if (input1 > rnd.Next(1, 100))
+                {
+                    output1TextBox.Text = "Guess lower";
+                }
+                else
+                {
+                    output1TextBox.Text = "Guess higher";
+                }
+
+            }
+            
+
+
+
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void endButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
